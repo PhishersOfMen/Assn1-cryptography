8842f93872a31a5ebc9577f5b6b7ecb7bf34c0f4
Michael A. Warren <mawarren24@gmail.com>
2019-09-09 17:50:27 -0600

    Dev 201909091750

--- oneTimePad.py
+++ oneTimePad.py
@@ -1,13 +1,26 @@
 def xor(binary, key):
-    rvalue = "10101010"
+    key = bin(key)
+    key = key[2:]
+    # TODO: padding for binary
+    # TODO: loop through key again if short
+    rvalue = ''
+    for i in range(len(key)):
+        if key[i] == binary[i]:
+            rvalue = f'{rvalue}0'
+        else:
+            rvalue = f'{rvalue}1'
     return rvalue
 
 def oneTimePad(value, key):
-    binary = bin(value)
-    binary = binary[2:]
+    binArray = []
+    for el in value:
+        temp = bin(el)
+        temp = temp[2:]
+        binArray.append(temp)
 
-    rvalue = xor(binary, key)
-
-    results = int(rvalue, 2)
+    results = []
+    for el in binArray:
+        rvalue = xor(el, key)
+        results.append(int(rvalue,2))
 
     return results
