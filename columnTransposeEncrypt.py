7026204cbe1b6edb110976a8a415b86ad959c386
Christian <cjl2311@gmail.com>
2019-09-10 15:34:34 -0600

    added columnTransposeEncrypt function

--- columnTransposeEncrypt.py
+++ columnTransposeEncrypt.py
@@ -1,4 +1,23 @@
-def columnTransposeEncrypt(text, key): 
-  # Takes in plain text and key generated from poly square
-  # parse key and number each column by alphabetical order
-  # 
+import textwrap
+
+
+def columnTransposeEncrypt(text, key):
+    text = text.replace(" ", "")
+    key = key.replace(" ", "")
+    rowList = textwrap.wrap(text, len(key))
+
+    valueList = []
+    for k in key:
+        valueList.append({"key": k, "vals": []})
+    for i in rowList:
+        for j in range(len(i)):
+            valueList[j]["vals"].append(i[j])
+
+    valueList = sorted(valueList, key=lambda k: k["key"])
+    ciphertext = ""
+    for i in valueList:
+        print(i)
+        ciphertext += str("".join(i["vals"]))
+
+    return ciphertext
+
