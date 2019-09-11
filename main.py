b3f6e4ec340084a6dd64c071cfb8184ebcb043b2
Michael A. Warren <mawarren24@gmail.com>
2019-09-11 12:55:55 -0600

    Dev Checkpoint 201909111255
    Basic Implementation of main.py

--- main.py
+++ main.py
@@ -1,13 +1,30 @@
 import sys
 
 from Config import config
+from columnTransposeDecrypt import columnTransposeDecrypt
+from columnTransposeEncrypt import columnTransposeEncrypt
 from oneTimePad import oneTimePad
+from polybius import polybiusToStr, polybiusToNum
 
 while True:
+    # User Input
     encrypt, plaintext, key = config()
+    otpKey = key[-2:]
+    key = key[:-2]
 
+    # Encryption Path
     if encrypt:
-        # encrypt path
+        polyKey = polybiusToStr(key)
+        ciphertext = columnTransposeEncrypt(plaintext, polyKey)
+        polyNums = polybiusToNum(ciphertext)
+        results = oneTimePad(polyNums, otpKey)
+        # TODO: merge results
+        print(results)
     
+    # Decryption Path
     else:
-        # decrypt path
+        results = oneTimePad(plaintext, otpKey)
+        # TODO: merge results
+        polyKey = polybiusToStr(results)
+        message = columnTransposeDecrypt(polyKey, key) # TODO Check
+        print(message)
