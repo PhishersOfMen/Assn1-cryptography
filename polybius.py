64db93be3b4021475b2cc1065c58952656221ae5
Ryan Medlyn <ryan.medlyn@gmail.com>
2019-09-10 22:44:21 -0600

    implement polybius functions

--- polybius.py
+++ polybius.py
@@ -1,1 +1,29 @@
-# TODO: Set square
+POLYBIUS = [
+    ["a", "f", "e", "d", "x"],
+    ["h", "j", "o", "u", "s"],
+    ["t", "d", "p", "y", "g"],
+    ["c", "r", "l", "i", "n"],
+    ["q", "k", "b", "m", "w"]
+]
+
+def polybiusToNum(letterStr):
+    letterStr.replace(" ", "")
+    nums = []
+    for c in letterStr:
+        for rowIndex, row in enumerate(POLYBIUS):
+            try:
+                colIndex = row.index(c)
+                nums.extend([str(rowIndex + 1), str(colIndex + 1)])
+                
+            except ValueError:
+                pass
+    return "".join(nums)
+
+def polybiusToStr(numStr):
+    print(numStr)
+    chars = []
+    for i in range(0, len(numStr), 2):
+        row = int(numStr[i]) - 1
+        col = int(numStr[i+1]) - 1
+        chars.append(POLYBIUS[row][col])
+    return "".join(chars)
